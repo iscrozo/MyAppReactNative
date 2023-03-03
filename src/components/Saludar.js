4d8db72e27e5acebc14c6efa2fbfb0d90a089a0e
“Sebastian <iscrozo@gmail.com>
2023-03-02 23:39:27 -0500

    uso de defaultprops

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -3,9 +3,19 @@ import {Text} from "react-native";
 
 
 export default function Saludar(props) {
-    const {name = "Jose", edad = "30"} = props;
+    //const {name = "Jose", edad = "30"} = props;
+
+    const { name, edad } = props;
     console.log(name);
     return (
         <Text>Hola {name}, tiene {edad} años</Text>
     )
 }
+
+
+Saludar.defaultProps = {
+    //firstname: "Agustin", 
+    //lastname: "Navarro"
+    name : "juan", 
+    edad : "15"
+};

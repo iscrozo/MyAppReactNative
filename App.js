cc4fb03f71cc9258ce8c825d12b3a48400ee7b5f
“Sebastian <iscrozo@gmail.com>
2023-03-03 23:00:31 -0500

    navigation with tabs

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import { StyleSheet } from "react-native";
 // importar el nuevo componente
 import { NavigationContainer } from "@react-navigation/native";
 import NavigationStack from "./src/navigation/NavigationStack";
+import NavigationTab from "./src/navigation/NavigationTab";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NavigationStack></NavigationStack>
+      {/*<NavigationStack></NavigationStack>*/}
+      <NavigationTab></NavigationTab>
     </NavigationContainer>
   );
 }

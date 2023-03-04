7ce1ac11162b9b01babff28ea36864aba7500920
“Sebastian <iscrozo@gmail.com>
2023-03-04 15:50:37 -0500

    test with drawer navigation

--- App.js
+++ App.js
@@ -6,12 +6,16 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import NavigationStack from "./src/navigation/NavigationStack";
 import NavigationTab from "./src/navigation/NavigationTab";
+import NavigationDrawer from "./src/navigation/NavigationDrawer";
+
+//import NavigationDrawer from "./src/navigation/NavigationDrawer";
 
 export default function App() {
   return (
     <NavigationContainer>
       {/*<NavigationStack></NavigationStack>*/}
-      <NavigationTab></NavigationTab>
+      {/* <NavigationTab></NavigationTab> */}
+      <NavigationDrawer />
     </NavigationContainer>
   );
 }
